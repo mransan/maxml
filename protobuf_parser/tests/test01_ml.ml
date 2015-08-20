5ef665d53a996a1a749ef8c83646ab8fec0efde4
mransan <maxime.ransan@gmail.com>
2015-08-19 21:23:00 -0400

    Refactor OCaml test file with a new Test_util module

--- protobuf_parser/tests/test01_ml.ml
+++ protobuf_parser/tests/test01_ml.ml
@@ -2,27 +2,6 @@
 module Pc = Protobuf_codec 
 module T  = Test01_types 
 
-module Task_util = struct 
-  type mode = 
-    | Encode 
-    | Decode 
-
-  let mode_of_string  = function 
-    | "encode" -> Encode
-    | "decode" -> Decode
-    | _ -> failwith "Invalid mode, must be [encode|decode]"
-
-  let parse_args () = 
-    let mode          = ref "" in  
-    let cmd_line_args = [ ] in 
-    let anon_fun  = (fun arg_mode -> 
-      mode := arg_mode
-    )  in 
-    let usage = "test01_ml.tsk [encode|decode]" in  
-    Arg.parse cmd_line_args anon_fun usage;
-    mode_of_string !mode 
-
-end 
 
 let decode_ref_data () = {
     T.p1 = {
@@ -48,47 +27,15 @@ let decode_ref_data () = {
     } :: []
   } 
 
-let decode () = 
-  let ic     = open_in_bin "test01.c2ml.data" in 
-  let buffer = Bytes.create 1024 in 
-  let size   = ref 0 in 
-  try 
-    while true do  
-      Bytes.set buffer !size (input_char ic); 
-      size := !size + 1
-    done
-  with | End_of_file -> (); 
-  Printf.printf "Done reading data, size=%i\n" !size ;
-
-  let buffer = Bytes.sub buffer 0 !size in 
-
-  let decoder = Pc.Decoder.of_bytes buffer in 
-  let cp = T.decode_couple decoder in  
-  print_endline @@ T.string_of_couple cp;
-  if cp = decode_ref_data () 
-  then (
-    print_endline "-- Good --"; 
-    exit 0
-  )
-  else (
-    print_endline "-- Test Failed --";  
-    print_endline @@ T.string_of_couple cp;
-    exit 1
-  )
-
-let encode () = 
-  let cp = decode_ref_data () in 
-  let encoder = Pc.Encoder.create () in 
-  T.encode_couple cp encoder; 
-  let oc = open_out_bin "test01.ml2c.data" in 
-  output_bytes oc @@ Pc.Encoder.to_bytes encoder 
    
 
 let () = 
 
-  let mode   = Task_util.parse_args () in 
+  let mode   = Test_util.parse_args () in 
 
   match mode with 
-  | Task_util.Decode -> decode () 
-  | Task_util.Encode -> encode ()
+  | Test_util.Decode -> 
+      Test_util.decode "test01.c2ml.data" T.decode_couple T.string_of_couple (decode_ref_data  ()) 
+  | Test_util.Encode -> 
+      Test_util.encode "test01.ml2c.data" T.encode_couple (decode_ref_data ())
 
--- protobuf_parser/tests/test02_ml.ml
+++ protobuf_parser/tests/test02_ml.ml
@@ -2,28 +2,6 @@
 module Pc = Protobuf_codec 
 module T  = Test02_types 
 
-module Task_util = struct 
-  type mode = 
-    | Encode 
-    | Decode 
-
-  let mode_of_string  = function 
-    | "encode" -> Encode
-    | "decode" -> Decode
-    | _ -> failwith "Invalid mode, must be [encode|decode]"
-
-  let parse_args () = 
-    let mode          = ref "" in  
-    let cmd_line_args = [ ] in 
-    let anon_fun  = (fun arg_mode -> 
-      mode := arg_mode
-    )  in 
-    let usage = "test01_ml.tsk [encode|decode]" in  
-    Arg.parse cmd_line_args anon_fun usage;
-    mode_of_string !mode 
-
-end 
-
 let decode_ref_data () = {
   T.o01 = 1.0;
   T.o02 = 2.0;
@@ -44,56 +22,13 @@ let decode_ref_data () = {
   T.o15 = "Iam a test byte"; 
 }
 
-
-let inc_size = 16 
-
-let get_binary_file_content file_name = 
-  let ic     = open_in_bin file_name in 
-  let rec loop b offset remaining = 
-    match input ic b offset remaining with
-    | 0 -> (b, offset)
-    | i when i = remaining -> (
-      let b = Bytes.extend b 0 inc_size in 
-      loop b (offset + remaining) inc_size
-    )
-    | i -> (
-      (b, offset + i) 
-    ) 
-  in 
-  loop (Bytes.create inc_size) 0 inc_size
-
-
-let decode () = 
-  let buffer, size = get_binary_file_content "test02.c2ml.data" in 
-  Printf.printf "Done reading data, size=%i\n" size ;
-
-  let buffer = Bytes.sub buffer 0 size in 
-  let decoder = Pc.Decoder.of_bytes buffer in 
-  
-  let abt = T.decode_allbasicstypes decoder in  
-  if abt = decode_ref_data () 
-  then (
-    print_endline "ML: -- Good --"; 
-    exit 0
-  )
-  else (
-    print_endline "ML: -- Test Failed --";  
-    print_endline @@ T.string_of_allbasicstypes abt;
-    exit 1
-  )
-
-let encode () = 
-  let abt = decode_ref_data () in 
-  let encoder = Pc.Encoder.create () in 
-  T.encode_allbasicstypes abt encoder; 
-  let oc = open_out_bin "test02.ml2c.data" in 
-  output_bytes oc @@ Pc.Encoder.to_bytes encoder 
-   
 let () = 
 
-  let mode   = Task_util.parse_args () in 
+  let mode   = Test_util.parse_args () in 
 
   match mode with 
-  | Task_util.Decode -> decode () 
-  | Task_util.Encode -> encode ()
+  | Test_util.Decode -> 
+      Test_util.decode "test02.c2ml.data" T.decode_allbasicstypes T.string_of_allbasicstypes (decode_ref_data ()) 
+  | Test_util.Encode -> 
+      Test_util.encode "test02.ml2c.data" T.encode_allbasicstypes (decode_ref_data ())
 
